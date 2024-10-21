f86e7b98850de01bdaebf88f2fe3dff29af7cebd
Quintin Stewart <108433446+Quintentional@users.noreply.github.com>
2024-10-20 22:01:57 -0400

    javascript complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-alert("I'm working.");
+const number = document.getElementsByClassName('number');
+const span = document.querySelector('span');
+const submit = document.querySelector(".submit");
+const rating = document.querySelector(".rating");
+const thankYou = document.querySelector('.thank-you');
+
+for (i = 0; i < number.length; i++) {
+    number[i].addEventListener('click', function() {
+        span.textContent=this.textContent;
+    })
+}
+
+submit.addEventListener('click', function() {
+    rating.classList.add("off");
+    rating.classList.remove("on");
+    thankYou.classList.add("on");
+    thankYou.classList.remove("off");
+})
